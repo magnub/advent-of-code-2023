db716a1fcae5b33c3481c7d35ac668da9effe84b
Magnus Borgström <220939@skane.se>
2023-12-04 00:17:17 +0100

    Solutions for day 1 & 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,63 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val lines = File("/Users/mange/Downloads/advent23/1").readLines()
+    part1(lines)
+    part2(lines)
 }
+
+fun sum(lines: List<String>): Int {
+    var sum = 0
+    lines.forEach {
+        if (it.isEmpty()) return@forEach
+        val first = it.first { it.isDigit() }
+        val last = it.last { it.isDigit() }
+        sum += "$first$last".toInt()
+    }
+    return sum
+}
+
+fun part1(lines: List<String>) {
+    println(sum(lines))
+}
+
+val numbers = setOf(
+    "zero" to "0",
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9"
+)
+
+fun part2(lines: List<String>) {
+    val list = mutableListOf<String>()
+    lines.forEach {
+        val windowed = it.windowed(5, 1, true)
+        var result = ""
+        windowed.forEach { each ->
+            each.trim().toNumber().filterNot { it.isWhitespace() || it.toString().isEmpty() }.let {
+                result += it
+            }
+        }
+        list.add(result)
+    }
+    println(sum(list))
+}
+
+fun String.toNumber(): String {
+    numbers.forEach {
+        if (this.contains(it.first)) {
+            return this.replace(it.first, it.second)
+        }
+    }
+    if (this.any { it.isDigit() }) {
+        return this
+    }
+    return ""
+}
+
---
+++ src/Day02.kt
@@ -0,0 +1,62 @@
+package twentythree
+
+import java.io.File
+import kotlin.math.max
+
+fun main() {
+    val lines = File("/Users/mange/Downloads/advent23/2").readLines()
+    part1(lines)
+    part2(lines)
+}
+
+data class Config(
+    val red: Int = 12,
+    val green: Int = 13,
+    val blue: Int = 14,
+)
+
+fun part1(lines: List<String>) {
+    val config = Config()
+    val results = mutableSetOf<Int>()
+    lines.forEach {
+        val noWhiteSpace = it.filterNot { it.isWhitespace() }.split("Game", ";", ",", ":")
+        val id = noWhiteSpace[1].toInt()
+        for (i in 2..<noWhiteSpace.size) {
+            if (noWhiteSpace[i].countIsValid(config)) {
+                results.add(id)
+            } else {
+                results.remove(id)
+                break
+            }
+        }
+    }
+    println(results.sum())
+}
+
+fun part2(lines: List<String>) {
+    val colors = setOf("red", "green", "blue")
+    var sum = 0
+    lines.forEach {
+        val noWhiteSpace = it.filterNot { it.isWhitespace() }.split("Game", ";", ",", ":")
+        val map = hashMapOf<String, Int>()
+        for (i in 2..<noWhiteSpace.size) {
+            colors.filter { noWhiteSpace[i].contains(it) }
+                .map { map[it] = max(noWhiteSpace[i].replaceToInt(it), map[it] ?: 0) }
+        }
+        sum += map.values.reduce { acc, value -> acc * value }
+    }
+    println(sum)
+}
+
+fun String.countIsValid(config: Config): Boolean {
+    return when {
+        this.contains("red") -> this.replaceToInt("red") <= config.red
+        this.contains("green") -> this.replaceToInt("green") <= config.green
+        this.contains("blue") -> this.replaceToInt("blue") <= config.blue
+        else -> {
+            false
+        }
+    }
+}
+
+fun String.replaceToInt(replace: String, newValue: String = "") = this.replace(replace, newValue).trim().toInt()
